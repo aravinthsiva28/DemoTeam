7d2f7cc7873bd26a4537d11b552328b1b7a40e76
aravinthsiva28 <aravinthsiva28@gmail.com>
2022-09-18 13:17:51 +0530

    Add project files.

---
+++ DemoTeamTest/CalculateTest.cs
@@ -0,0 +1,20 @@
+﻿using DemoTeam.Controllers;
+using NUnit.Framework;
+
+namespace DemoTeamTest
+{
+    [TestFixture]
+    public class CalculateTest
+    {
+        [TestCase(10,2,5)]
+        [TestCase(1,0,0)]
+        public void CalculateValidation(int a,int b,decimal res)
+        {
+            var test = new ValuesController();
+
+            decimal val = test.Calculate(a, b);
+
+            Assert.AreEqual(res, val);
+        }
+    }
+}
